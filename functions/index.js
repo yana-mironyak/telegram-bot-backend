24d9b9acb5820ea99e2e57d40db87497c9652384
Yana Mironyak <yanika.mironyak@gmail.com>
2023-05-24 17:59:41 +0300

    feat: add files

---
+++ functions/index.js
@@ -0,0 +1,52 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+
+const cors = require("cors")({ origin: true });
+app.use(cors);
+
+app.get("/", (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
+  res.send(`
+    <!doctype html>
+    <head>
+      <title>Time</title>
+      <link rel="stylesheet" href="/style.css">
+      <script src="/script.js"></script>
+    </head>
+    <body>
+      <p>In London, the clock strikes:
+        <span id="bongs">${"BONG ".repeat(hours)}</span></p>
+      <button onClick="refresh(this)">Refresh</button>
+    </body>
+  </html>`);
+});
+
+app.get("/api", (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
+  res.json({ bongs: "BONG ".repeat(hours) });
+});
+
+exports.app = functions.https.onRequest(app);
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
---
+++ index.js
@@ -0,0 +1,49 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "5905362747:AAH5rKswHEYUyVVNihVcVR8HDSNo5sNlWHY";
+
+const bot = new TelegramBot(token, { polling: true });
+const options = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        { text: "Бухгалтерія", callback_data: "Помилка оформлення" },
+        { text: "ЮД", callback_data: "Гарантійний лист" },
+      ],
+      [{ text: "Фітнес", callback_data: "Відміна заняття" }],
+    ],
+  }),
+};
+
+const start = () => {
+  bot.setMyCommands([
+    { command: "/start", description: "Let`s go" },
+    { command: "/info", description: "Categories" },
+  ]);
+
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === "/start") {
+      return bot.sendMessage(chatId, "Шо треба?");
+    }
+
+    if (text === "/info") {
+      return bot.sendMessage(chatId, "Обери категорію", options);
+    }
+
+    console.log(msg);
+    return bot.sendMessage(chatId, "Якась хуйня");
+  });
+
+  bot.on("callback_query", (msg) => {
+    console.log(msg);
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+
+    bot.sendMessage(chatId, `${data}`);
+  });
+};
+
+start();
