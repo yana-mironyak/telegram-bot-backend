5e063b1ce6b54f3a498dc4539d66c476a850f3f3
Yana Mironyak <yanika.mironyak@gmail.com>
2023-05-25 18:48:13 +0300

    feat: delete firebase

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {
+  polling: {
+    interval: 1000,
+  },
+});
 const options = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
