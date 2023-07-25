a1a726810ff00c4f6601a8ad3c58365c5fa780f4
Yana Mironyak <yanika.mironyak@gmail.com>
2023-07-25 22:37:11 +0300

    feat: delete googleAuth

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import TelegramBot from "node-telegram-bot-api";
 import * as dotenv from "dotenv";
-import handlebars from "handlebars";
-import fs from "fs";
-import nodemailer from "nodemailer";
+// import handlebars from "handlebars";
+// import fs from "fs";
+// import nodemailer from "nodemailer";
 // import { GoogleAuth } from "google-auth-library";
 
 dotenv.config();
 
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-const templateSource = fs.readFileSync("emailTemplate.hbs", "utf8");
-const template = handlebars.compile(templateSource);
+// const templateSource = fs.readFileSync("emailTemplate.hbs", "utf8");
+// const template = handlebars.compile(templateSource);
+// const encodedTemplate = Buffer.from(templateSource).toString("base64");
+// console.log(encodedTemplate);
 
 const options = {
   reply_markup: JSON.stringify({
@@ -75,43 +77,40 @@ const fitness = {
 };
 
 const start = async () => {
-  process.env.GOOGLE_APPLICATION_CREDENTIALS =
-    "./manual-sl-bot-af6d6e6bf571.json";
-
   // const auth = new GoogleAuth();
   // const client = await auth.getClient();
 
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      type: "OAuth2",
-      user: "sportlife.manual@gmail.com",
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: process.env.ACCESS_TOKEN,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   service: "Gmail",
+  //   host: "smtp.gmail.com",
+  //   port: 587,
+  //   secure: false,
+  //   auth: {
+  //     type: "OAuth2",
+  //     user: "sportlife.manual@gmail.com",
+  //     clientId: process.env.CLIENT_ID,
+  //     clientSecret: process.env.CLIENT_SECRET,
+  //     refreshToken: process.env.REFRESH_TOKEN,
+  //     accessToken: process.env.ACCESS_TOKEN,
+  //   },
+  // });
 
-  function sendEmailWithTemplate(to, subject, message) {
-    const data = {
-      subject: "Тема листа",
-      message: "Текст листа",
-    };
+  // function sendEmailWithTemplate(to, subject, message) {
+  //   const data = {
+  //     subject: "Тема листа",
+  //     message: "Текст листа",
+  //   };
 
-    const emailBody = template(data);
+  //   const emailBody = template(data);
 
-    transporter.sendMail({ to, subject, html: emailBody }, (error, info) => {
-      if (error) {
-        console.log("Помилка при відправленні листа:", error);
-      } else {
-        console.log("Лист відправлено:", info.response);
-      }
-    });
-  }
+  //   transporter.sendMail({ to, subject, html: emailBody }, (error, info) => {
+  //     if (error) {
+  //       console.log("Помилка при відправленні листа:", error);
+  //     } else {
+  //       console.log("Лист відправлено:", info.response);
+  //     }
+  //   });
+  // }
 
   bot.setMyCommands([
     { command: "/start", description: "Let`s go" },
@@ -121,6 +120,8 @@ const start = async () => {
   bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
+
+    console.log(text);
 
     if (text === "/start") {
       return bot.sendMessage(chatId, "Шо вже трапилось?");
@@ -150,20 +151,14 @@ const start = async () => {
     const chatId = query.message.chat.id;
 
     if (data === "Відміна запізення на пошті") {
-      // return bot.sendMessage(
-      //   chatId,
-      //   "Адресати: example@example\\.com, example@example\\.com, example@example\\.com\n\nТема листа: Ситуація з клієнтом, ПОД, ПІБ\n\nТіло листа ↓↓↓\n\n*Ситуація:*\n*Завдання:*\n\n*Інформація про клієнта:*\nПІБ:\nКатегорія картки:\nКод анкети:",
-      //   {
-      //     parse_mode: "MarkdownV2",
-      //   }
-      // );
+      bot.sendMessage(chatId, "Напиши свою пошту, я відправлю тобі шаблон");
       const to = "mironiak.yana@sportlife.kiev.ua";
       const subject = "Відміна запізнення, ПОД, ПІБ";
       const message = "Текст листа";
 
       sendEmailWithTemplate(to, subject, message);
     }
-    console.log(data);
+
     bot.sendMessage(chatId, `${data}`);
   });
 };
